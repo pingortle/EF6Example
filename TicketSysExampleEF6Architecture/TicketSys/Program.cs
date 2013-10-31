3c7d6680fda23f583d806d57320515d4a9d8888d
pingortle <pingortle@gmail.com>
2013-10-31 16:08:18 -0500

    Grossly abuse ninject.

--- TicketSysExampleEF6Architecture/TicketSys/Program.cs
+++ TicketSysExampleEF6Architecture/TicketSys/Program.cs
@@ -5,57 +5,73 @@ using System.Text;
 using System.Threading.Tasks;
 using TicketSys.Domain;
 using TicketSys.Persistence;
+using Ninject;
+using Ninject.Extensions.NamedScope;
 
 namespace TicketSys
 {
+    class DoStuff
+    {
+        IRepository repo;
+
+        public DoStuff(IRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Now()
+        {
+                        int count = 0;
+            foreach (Type t in repo.GetAvailableTypes())
+            {
+                Console.WriteLine(" {0}\t{1}", (++count).ToString(), t.ToString());
+            }
+
+            IStore<Customer> customers = repo.GetStoreOf<Customer>();
+            using (customers.ScopedChanges())
+            {
+                customers.Add(new Customer { BusinessName = "Test", ContactName = "Test", Phone = "+X (XXX) XXX XXXX" });
+                customers.Add(new Customer { BusinessName = "Test2", ContactName = "Test", Phone = "+X (XXX) XXX XXXX" });
+                customers.Add(new Customer { BusinessName = "Test3", ContactName = "Test", Phone = "+X (XXX) XXX XXXX" });
+                customers.Add(new Customer { BusinessName = "Test", ContactName = "Test", Phone = "+X (XXX) XXX XXXX" });
+            }
+
+            Console.WriteLine("");
+
+            foreach (Customer c in repo.GetStoreOf<Customer>())
+            {
+                Console.WriteLine(c.BusinessName);
+            }
+
+            Console.Write("Press any key to continue.");
+            Console.ReadKey();
+            Console.WriteLine();
+
+            Console.WriteLine("Rolling back changes...");
+
+            using (customers.ScopedChanges())
+            {
+                foreach (var c in customers
+                    .AsEnumerable()
+                    .Where(x =>
+                        x.ContactName.ToLower().Contains("test") ||
+                        x.BusinessName.ToLower().Contains("test") ||
+                        !x.Phone.Any(m => char.IsLetter(m))))
+                {
+                    customers.Remove(c);
+                }
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            using (IRepository repo = RepositoryFactory.MakeTicketingRepository())
-            {
+            var kernel = new StandardKernel();
 
-                int count = 0;
-                foreach (Type t in repo.GetAvailableTypes())
-                {
-                    Console.WriteLine(" {0}\t{1}", (++count).ToString(), t.ToString());
-                }
-
-                IStore<Customer> customers = repo.GetStoreOf<Customer>();
-                using (customers.ScopedChanges())
-                {
-                    customers.Add(new Customer { BusinessName = "Test", ContactName = "Test", Phone = "+X (XXX) XXX XXXX" });
-                    customers.Add(new Customer { BusinessName = "Test2", ContactName = "Test", Phone = "+X (XXX) XXX XXXX" });
-                    customers.Add(new Customer { BusinessName = "Test3", ContactName = "Test", Phone = "+X (XXX) XXX XXXX" });
-                    customers.Add(new Customer { BusinessName = "Test", ContactName = "Test", Phone = "+X (XXX) XXX XXXX" });
-                }
-
-                Console.WriteLine("");
-
-                foreach (Customer c in repo.GetStoreOf<Customer>())
-                {
-                    Console.WriteLine(c.BusinessName);
-                }
-
-                Console.Write("Press any key to continue.");
-                Console.ReadKey();
-                Console.WriteLine();
-
-                Console.WriteLine("Rolling back changes...");
-
-                using (customers.ScopedChanges())
-                {
-                    foreach (var c in customers
-                        .AsEnumerable()
-                        .Where(x =>
-                            x.ContactName.ToLower().Contains("test") ||
-                            x.BusinessName.ToLower().Contains("test") ||
-                            !x.Phone.Any(m => char.IsLetter(m))))
-                    {
-                        customers.Remove(c);
-                    }
-                }
-            }
+            kernel.Bind<IRepository>().To<TicketingRepository>().InParentScope();
+            kernel.Get<DoStuff>().Now();
 
             Console.WriteLine("Exiting...");
         }
